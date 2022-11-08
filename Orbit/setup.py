e689dded8951217e45134ebe3fad6c1829c208d3
jchaves1406 <84520072+jchaves1406@users.noreply.github.com>
2022-11-08 13:19:19 -0500

    Delete setup.py

--- Orbit/setup.py
+++
@@ -1,8 +0,0 @@
-# Fichero para la creación del objeto compartido
-
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-exts = (cythonize("orbit_cy.pyx"))
-
-setup(ext_modules = exts)
